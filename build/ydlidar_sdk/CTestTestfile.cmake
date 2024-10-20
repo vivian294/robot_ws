b05831011ea8465baff93268d9956770fb325047
vivian294 <vvspikey.29@gmail.com>
2024-10-21 01:25:01 +0800

    Updated YDLidar-SDK and ydlidar_ros2_driver; ignored build, install, and log directories

---
+++ build/ydlidar_sdk/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/robot_ws/src/YDLidar-SDK
+# Build directory: /home/robot/robot_ws/build/ydlidar_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("src")
+subdirs("examples")
---
+++ install/ydlidar_ros2_driver/share/ydlidar_ros2_driver/local_setup.bash
@@ -0,0 +1,1 @@
+/home/robot/robot_ws/build/ydlidar_ros2_driver/ament_cmake_environment_hooks/local_setup.bash
